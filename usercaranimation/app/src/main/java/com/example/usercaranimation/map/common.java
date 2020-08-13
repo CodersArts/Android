85b513842c8870b7a797a34d16cb970f7f4e16af
Dell <rishabh1422869singh@gmail.com>
2020-08-13 19:19:57 +0530

    used google map with retrofit Api

---
+++ usercaranimation/app/src/main/java/com/example/usercaranimation/map/common.java
@@ -0,0 +1,9 @@
+package com.example.usercaranimation.map;
+
+public class common {
+    public  static  String baseURL="https://googleapis.com";
+    public static interface1 GetInterface1()
+    {
+        return Retrofic.getClient(baseURL).create(interface1.class);
+    }
+}
